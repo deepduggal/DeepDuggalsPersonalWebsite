8683b994400cd226b58054ef28a93d064e7ef3f2
Deep Duggal <deepduggal57@yahoo.com>
2017-06-29 17:14:34 -0400

    Removed old project, updated link, fixed form

--- projects/branson/childhood/js/index.js
+++ projects/branson/childhood/js/index.js
@@ -72,6 +72,49 @@ story.addEventListener('mouseup', function(e){
   story.style.cursor = 'grab';
 });
 
+// Scroll via swipe
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            TweenMax.to(story, 0.3, {scrollLeft: '+=' + window.innerWidth/5});
+        } else {
+            /* right swipe */
+            TweenMax.to(story, 0.3, {scrollLeft: '-=' + window.innerWidth/5});
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
 // Turn sounds on or off
 musicOnOff.addEventListener('click', function() {
   // Pause
